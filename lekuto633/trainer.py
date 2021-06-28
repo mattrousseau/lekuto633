dd950515ae1b26aab48819e65818470913260dcf
Matthieu Rousseau <matthieurousseau86@gmail.com>
2021-06-28 09:54:38 +0200

    added standard scaler

--- lekuto633/trainer.py
+++ lekuto633/trainer.py
@@ -1,5 +1,6 @@
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 class Trainer():
     def train(self):
         ohe = OneHotEncoder()
+        std_scaler = StandardScaler()
