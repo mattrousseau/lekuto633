f500445d6e72797fc581356ac477fbf2e8d72a97
Matthieu Rousseau <matthieurousseau86@gmail.com>
2021-06-28 09:56:25 +0200

    added linear reg

--- lekuto633/trainer.py
+++ lekuto633/trainer.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
 
 class Trainer():
     def train(self):
         ohe = OneHotEncoder()
+        lr = LinearRegression()
